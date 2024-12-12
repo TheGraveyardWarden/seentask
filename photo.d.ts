e2297911869f4dfe1a34a2ff6a6dc745bdfb2704
TheGraveyardWarden <alimanutd10r@gmail.com>
2024-12-12 05:17:05 +0330

    fix

---
+++ photo.d.ts
@@ -0,0 +1,19 @@
+declare module "*.jpg" {
+    const value: any;
+    module.exports = value;
+}
+
+declare module "*.png" {
+    const value: any;
+    export = value;
+}
+
+declare module "*.svg" {
+    const value: any;
+    export = value;
+}
+
+declare module "*.webp" {
+    const value: any;
+    export = value;
+}
